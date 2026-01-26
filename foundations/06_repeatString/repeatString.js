a4bfa37518b3908beee141df70fb5eac05f165e5
Eudesto <chamyklicaros11@gmail.com>
2026-01-27 04:36:19 +0800

    finish 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+function repeatString(string,num)
+{
+    let word = ""
+    if (num < 0 )
+    {
+        return "ERROR";
+    }
 
-};
+    for (let i = 0; i < num; i++)
+    {
+        word += string;
+    }
+
+    return word;
+
+}
+
+repeatString("hey",3)
+
 
 // Do not edit below this line
 module.exports = repeatString;
