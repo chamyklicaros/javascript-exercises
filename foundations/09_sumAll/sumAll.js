bc6320e159ea2be6c7b0dd3b14f3cd2417edf801
Eudesto <chamyklicaros11@gmail.com>
2026-01-27 22:39:30 +0800

    finished 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,29 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+    let sum = 0;
+    let swap;
+
+
+    if (min > max){
+        swap = max;
+        max = min;
+        min = swap;
+    }
+
+    if (min < 0 || max < 0){
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+
+    for (let i = min; i <= max; i++ )
+    {
+        sum += i;
+
+    }
+
+    return sum;
 
 };
 
