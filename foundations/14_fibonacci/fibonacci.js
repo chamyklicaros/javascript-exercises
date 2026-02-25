35019fae8ad7254c23a659f3d2dd9b739c73adf4
Eudesto <chamyklicaros11@gmail.com>
2026-02-25 20:37:44 +0800

    finished 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+function fibonacci(n){
+    if (n == 0){
+        return 0;
+    }
+    if (n == 1){
+        return 1;
+    }
+    if (n < 0){
+        return "OOPS";
+    }
 
-};
-
+    return fibonacci(n-1) + fibonacci(n-2);
+}
 // Do not edit below this line
 module.exports = fibonacci;
