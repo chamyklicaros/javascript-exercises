3d3223c06355c2382ca47b28a25d8cd2b3d88cab
Eudesto <chamyklicaros11@gmail.com>
2026-02-25 19:43:08 +0800

    finished calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,27 +1,37 @@
-const add = function() {
-	
-};
+function add(a, b) {
+  return a + b;
+}
+function subtract(a,b){
+  return a-b;
+}
+function sum (arr){
+  sum = 0
+  for (let i = 0; i <arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
 
-const subtract = function() {
-	
-};
+}
 
-const sum = function() {
-	
-};
+function multiply(arr){
+  let product = 1;
+    for (let i = 0; i <arr.length; i++){
+      product *= arr[i];
+    }
+  return product;
+}
 
-const multiply = function() {
+function power(Base,exponent){
+  return Base ** exponent;
 
-};
+}
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
-
+function factorial(n){
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
 // Do not edit below this line
 module.exports = {
   add,
