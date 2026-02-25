873c3c8af8e5233375181ac000f316cde262d6ad
Eudesto <chamyklicaros11@gmail.com>
2026-02-25 20:02:38 +0800

    finished 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+function palindromes(string) {
+    let stringarr = string.toLowerCase().replace(/[^a-z0-9]/g, '').split('');
+    let cleanedstr = stringarr.join('');
+    let reversed = stringarr.reverse().join('');
+  
+    
+    if (cleanedstr === reversed) {
+        return true;
+    } else {
+        return false;
+    }
 
-};
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
