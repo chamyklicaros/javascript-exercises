ab634172872ea0bdc8ac3a7cd9f984264169f4aa
Eudesto <chamyklicaros11@gmail.com>
2026-02-25 23:05:54 +0800

    finished

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
+function findTheOldest(people) {
+    const currenYear = new Date().getFullYear();
+    return people.reduce((oldest, person) => {
+        const oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currenYear - oldest.yearOfBirth;
+        const personAge = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : currenYear - person.yearOfBirth;
+        return personAge > oldestAge ? person : oldest;
+    })
 
-};
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
